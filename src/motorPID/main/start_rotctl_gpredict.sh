dc240473591ccfdc47806f5124f59b3518003823
fadhillaxl <fadhilla.php@gmail.com>
2026-04-25 03:15:14 +0700

    Update start_rotctl_gpredict.sh

--- src/motorPID/main/start_rotctl_gpredict.sh
+++ src/motorPID/main/start_rotctl_gpredict.sh
@@ -6,12 +6,8 @@ cd "${SCRIPT_DIR}"
 
 LISTEN_PORT="${LISTEN_PORT:-4533}"
 LISTEN_HOST="${LISTEN_HOST:-0.0.0.0}"
-TARGET_AZ="${TARGET_AZ:-20.0}"
-TARGET_EL="${TARGET_EL:-70.0}"
 
 python3 az_el_controller.py \
   --mode rotctl \
   --rotctl-host "${LISTEN_HOST}" \
-  --rotctl-port "${LISTEN_PORT}" \
-  --az "${TARGET_AZ}" \
-  --el "${TARGET_EL}"
+  --rotctl-port "${LISTEN_PORT}"
